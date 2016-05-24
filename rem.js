fae7138bfa37017725602b0effc4cd7370e43f95
kuangqijie <635389271@qq.com>
2016-05-24 16:51:46 +0800

    1

--- rem.js
+++ rem.js
@@ -1,16 +1,16 @@
-(function(w){
-    s();
-    function s() {
-        var width = w.innerWidth;
-        var a = width / 640;
-        if(a>1) a = 1;
-        if(a<0.5) a = 0.5;
-        document.querySelector('html').style.fontSize = a*100+'px';
+/*
+* 1rem = 100px js解决方案
+* */
+(function(win, doc, w){
+    var ev = 'onorientationchange' in window ? 'orientationchange': 'resize';
+    function setFs(){
+        var clientWidth = win.innerWidth;
+        if(clientWidth>w) clientWidth = w;
+        if(clientWidth<320) clientWidth = 320;
+
+        doc.querySelector('html').style.fontSize = clientWidth/w*100+'px';
     }
-    var b = ("onorientationchange" in w) ? "orientationchange": "resize";
-    w.addEventListener(b, function() {
-        setTimeout(function() {
-            s()
-        }, 1000 / 30)
-    }, false)
-})(window);
+
+    win.addEventListener(ev, setFs, false);
+    doc.addEventListener('DOMContentLoaded', setFs, false);
+})(window, document, 750);
